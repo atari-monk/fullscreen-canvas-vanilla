e71d477fa44d59e79a58eb5aae04bbe54291dfa3
atari-monk <atari.monk1@gmail.com>
2025-08-23 22:01:30 +0200

    Bump version to 0.0.10, Remove old log

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -24,7 +24,6 @@ export class Renderer {
 
     public start(): void {
         if (this.isRunning) return;
-        console.log("start");
         this.isRunning = true;
         this.timeCalculator.reset();
         this.rafId = requestAnimationFrame(this.frameTick.bind(this));
